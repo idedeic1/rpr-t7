2a16a9e38fa9289ecd989b60bdc147c971ea6908
Ibrahim Dedeic <idedeic1@etf.unsa.ba>
2018-12-05 11:58:16 +0100

    5

--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -1,8 +1,48 @@
 package ba.unsa.rpr.tutorijal7;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Tutorijal {
 
-    public static void main(String[] args) {
-	// write your code here
+    static ArrayList<Grad> ucitajGradove(String naziv){
+        Scanner input;
+        try {
+            input = new Scanner(new FileReader(naziv));
+        }catch(FileNotFoundException ex){
+            System.out.println("Greska" + ex);
+            return null;
+        }
+        ArrayList<String> tmp = new ArrayList<String>();
+
+        while(input.hasNext()){
+            tmp.add(input.nextLine());
+        }
+        ArrayList<Grad> gradovi = new ArrayList<Grad>();
+        for(int i=0; i<tmp.size(); i++){
+            Grad grad = new Grad();
+            double[] temp = new double[1000];
+            String[] nazivGrada = tmp.get(i).split(",");
+            grad.setNaziv(nazivGrada[0]);
+            for (int j = 1; j < nazivGrada.length; j++) {
+                try {
+                    temp[j - 1] = Double.valueOf(nazivGrada[j]);
+                } catch (ArrayIndexOutOfBoundsException e){
+                    return null;
+                }
+            }
+
+            grad.setTemperature(temp);
+            gradovi.add(grad);
+        }
+
+        return gradovi;
     }
+    
+    public static void main(String[] args) {
+
+    }
+
 }
